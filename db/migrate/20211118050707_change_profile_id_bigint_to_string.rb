9d4e3bc41c2b1d73a1ca8e336fe974ab0db5c566
Martin Olson <martinguyolson@gmail.com>
2021-11-19 10:31:03 +1100

    fixed flash messages, something about line endings

--- db/migrate/20211118050707_change_profile_id_bigint_to_string.rb
+++ db/migrate/20211118050707_change_profile_id_bigint_to_string.rb
@@ -1,5 +1,5 @@
 class ChangeProfileIdBigintToString < ActiveRecord::Migration[6.1]
   def change
-    change_column :profiles, :profile_id, :string
+    change_column :profiles, :profile_id, :string#this works, it might not be the best idea
   end
 end
